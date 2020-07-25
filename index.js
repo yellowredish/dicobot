71a6a8e6825f5b5e4731c3e4152c55fe338a00f8
yellowredish <gusgh3815@gmail.com>
2020-07-25 14:29:18 +0900

    add clear command

--- index.js
+++ index.js
@@ -75,50 +75,6 @@ if(message.content == 'embed') {
     .setTimestamp()
     .setFooter('노란색치킨무', img)
 
-  message.channel.send(embed)
 }
-commandList.forEach(x => {
-  commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
-});embed.addField('Commands: ', commandStr);
-
-message.channel.send(embed)
-
-
-if(message.content.startsWith('!전체공지')) {
-if(checkPermission(message)) return
-if(message.member != null) { // 채널에서 공지 쓸 때
-  let contents = message.content.slice('!전체공지'.length);
-  message.member.guild.members.array().forEach(x => {
-    if(x.user.bot) return;
-    x.user.send(`<@${message.author.id}> ${contents}`);
-  });
-
-  return message.reply('공지를 전송했습니다.');
-} else {
-  return message.reply('채널에서 실행해주세요.');
-}
-}
-
-function checkPermission(message) {
-if(!message.member.hasPermission("MANAGE_MESSAGES")) {
-message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
-return true;
-} else {
-return false;
-}
-}
-
-function changeCommandStringLength(str, limitLen = 8) {
-let tmp = str;
-limitLen -= tmp.length;
-
-for(let i=0;i<limitLen;i++) {
-  tmp += ' ';
-}
-
-return tmp;
-}
-
-
 
 client.login(token);

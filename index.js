ff81c2d9a5ac9381f10bc6fa4ca4a17d983cf3c6
yellowredish <gusgh3815@gmail.com>
2020-07-25 14:41:51 +0900

    add clear command

--- index.js
+++ index.js
@@ -58,25 +58,4 @@ client.on('message', (message) => {
 else if(message.content === '안녕')
 message.reply('안녕하세요 :D')
 });
-const Discord = require('노란색치킨무 봇');
-
-// inside a command, event listener, etc.
-const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('노란색 치킨무')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('매우깐지나는 봇')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-channel.send(exampleEmbed);
 client.login(token);

ddaaace557655d3fe4f2513c7dbbe81d97882956
yellowredish <gusgh3815@gmail.com>
2020-07-26 11:38:22 +0900

    add clear command

--- index.js
+++ index.js
@@ -52,12 +52,29 @@ client.on('message', (message) => {
   else if(message.content === 'tlqkf') {
     message.reply('착한말을 사용해주세요..');
   }
-  else if(message.content === 'lqkf') {
+  else if(message.content === 'Tlqkf') {
     message.reply('착한말을 사용해주세요..');
   }
-else if(message.content === '안녕')
+else if(message.content === '안녕') {
 message.reply('안녕하세요 :D')
-else if(message.content === ' 미안하다 이거 보여주려고 어그로끌었다.. 나루토 사스케 싸움수준 ㄹㅇ실화냐? 진짜 세계관최강자들의 싸움이다.. 그찐따같던 나루토가 맞나? 진짜 나루토는 전설이다..진짜옛날에 맨날나루토봘는데 왕같은존재인 호카게 되서 세계최강 전설적인 영웅이된나루토보면 진짜내가다 감격스럽고 나루토 노래부터 명장면까지 가슴울리는장면들이 뇌리에 스치면서 가슴이 웅장해진다..')
-message.reply('ㄹㅇㅋㅋ')
+}
+else if(message.content === '미안하다 이거 보여주려고 어그로끌었다.. 나루토 사스케 싸움수준 ㄹㅇ실화냐? 진짜 세계관최강자들의 싸움이다.. 그찐따같던 나루토가 맞나? 진짜 나루토는 전설이다..진짜옛날에 맨날나루토봘는데 왕같은존재인 호카게 되서 세계최강 전설적인 영웅이된나루토보면 진짜내가다 감격스럽고 나루토 노래부터 명장면까지 가슴울리는장면들이 뇌리에 스치면서 가슴이 웅장해진다..') {
+message.reply('ㄹㅇㅋㅋ') 
+}
+else if(message.content === '심준석') {
+  message.reply('최고존엄자..?')
+  }
+  else if(message.content === '히오스하자') {
+    message.reply('그망겜 왜함;;;')
+    }
+    else if(message.content === 'ㅎㅇ') {
+      message.reply('안녕하세요 :D')
+      }
+      else if(message.content === 'ㅋㅋㄹㅃㅃ') {
+        message.reply('쿠쿠루삥뽕')
+        }     
+          else if(message.content === '삔나') {
+          message.reply('삔나?')
+          } 
 });
 client.login(token);
